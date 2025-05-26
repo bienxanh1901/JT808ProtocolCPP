c237a9c0eb1a4a059645def1bf5cc8faf6dd1440
Pham Thanh Hai <haipt@vietmap.vn>
2025-05-26 10:23:43 +0700

    feat: #3 Add ElectronicWaybillReport Message Body

--- tests/DrivingRecordCommandTest.cpp
+++ tests/DrivingRecordCommandTest.cpp
@@ -3,26 +3,25 @@
 #include "MessageBodyBaseTest.h"
 
 namespace JT808::MessageBody {
-class DrivingRecordDataCollectionCommandTest : public MessageBodyBaseTest<DrivingRecordCommand>
+class DrivingRecordCommandTest : public MessageBodyBaseTest<DrivingRecordCommand>
 {
 protected:
-    ~DrivingRecordDataCollectionCommandTest() override { }
+    ~DrivingRecordCommandTest() override { }
 
     void SetUp() override
     {
-        m_body = new DrivingRecordCommand(0x01, "DrivingRecordDataCollectionCommand");
-        m_rawData = {0x1,  0x44, 0x72, 0x69, 0x76, 0x69, 0x6e, 0x67, 0x52, 0x65, 0x63, 0x6f,
-                     0x72, 0x64, 0x44, 0x61, 0x74, 0x61, 0x43, 0x6f, 0x6c, 0x6c, 0x65, 0x63,
-                     0x74, 0x69, 0x6f, 0x6e, 0x43, 0x6f, 0x6d, 0x6d, 0x61, 0x6e, 0x64};
+        m_body = new DrivingRecordCommand(0x01, "DrivingRecordCommand");
+        m_rawData = {0x1,  0x44, 0x72, 0x69, 0x76, 0x69, 0x6e, 0x67, 0x52, 0x65, 0x63,
+                     0x6f, 0x72, 0x64, 0x43, 0x6f, 0x6d, 0x6d, 0x61, 0x6e, 0x64};
     }
 };
 
-TEST_F(DrivingRecordDataCollectionCommandTest, TestParseSuccess)
+TEST_F(DrivingRecordCommandTest, TestParseSuccess)
 {
     TestParse();
 }
 
-TEST_F(DrivingRecordDataCollectionCommandTest, TestPackage)
+TEST_F(DrivingRecordCommandTest, TestPackage)
 {
     TestPackage();
 }
