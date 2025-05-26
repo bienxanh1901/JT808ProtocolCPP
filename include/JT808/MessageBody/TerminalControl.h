3ed73aa8777a3773b4a84a5bcfb624529590348e
Pham Thanh Hai <haipt@vietmap.vn>
2025-05-26 10:10:02 +0700

    feat: #3 Add DrivingRecords Message Bodys

--- include/JT808/MessageBody/TerminalControl.h
+++ include/JT808/MessageBody/TerminalControl.h
@@ -25,15 +25,6 @@ public:
         WirelessCommunicationOff = 7
     };
 
-    /**
-     * @brief ConnectionControlFlags
-     */
-    enum ConnectionControlFlags
-    {
-        SpecificPlatformServer = 0,
-        DefaultPlatformServer = 1
-    };
-
     TerminalControl() = default;
     TerminalControl(Commands command, const std::string& param = "");
     void parse(const std::vector<uint8_t>& data) override;
@@ -43,54 +34,12 @@ public:
 
     Commands command() const;
     void setCommand(Commands newCommand);
-
     std::string param() const;
     void setParam(const std::string& newParam);
-
-    ConnectionControlFlags connectionControl() const;
-    void setConnectionControl(ConnectionControlFlags newConnectionControl);
-    std::string serverAPN() const;
-    void setServerAPN(const std::string& newServerAPN);
-    std::string username() const;
-    void setUsername(const std::string& newUsername);
-    std::string password() const;
-    void setPassword(const std::string& newPassword);
-    std::string serverAddress() const;
-    void setServerAddress(const std::string& newServerAddress);
-    uint16_t tcpPort() const;
-    void setTcpPort(uint16_t newTcpPort);
-    uint16_t udpPort() const;
-    void setUdpPort(uint16_t newUdpPort);
-    std::vector<uint8_t> manufacturer() const;
-    void setManufacturer(const std::vector<uint8_t>& newManufacturer);
-    std::string authCode() const;
-    void setAuthCode(const std::string& newAuthCode);
-    std::string hwVersion() const;
-    void setHwVersion(const std::string& newHwVersion);
-    std::string fwVersion() const;
-    void setFwVersion(const std::string& newFwVersion);
-    std::string url() const;
-    void setUrl(const std::string& newUrl);
-    uint16_t connectionTimeout() const;
-    void setConnectionTimeout(uint16_t newConnectionTimeout);
 
 private:
     Commands m_command;
     std::string m_param;
-
-    ConnectionControlFlags m_connectionControl = SpecificPlatformServer;
-    std::string m_serverAPN;
-    std::string m_username;
-    std::string m_password;
-    std::string m_serverAddress;
-    uint16_t m_tcpPort = 0;
-    uint16_t m_udpPort = 0;
-    std::vector<uint8_t> m_manufacturer;
-    std::string m_authCode;
-    std::string m_hwVersion;
-    std::string m_fwVersion;
-    std::string m_url;
-    uint16_t m_connectionTimeout = 0;
 };
 
 }
